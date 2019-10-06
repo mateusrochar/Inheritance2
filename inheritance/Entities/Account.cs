0b7f2ea0f8d67916b3f970c9b4554d556067364f
mateusrochar <mateusrochar@gmail.com>
2019-10-06 16:57:52 -0300

    Add some changes in the main programan and in account properties

--- inheritance/Entities/Account.cs
+++ inheritance/Entities/Account.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace inheritance.Entities
 {
     class Account
     {
-        public int Number { get; set; }
-        public string Holder { get; set; }
-        public double Balance { get; set; }
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+
+        // protected permite alterar na subclass. No entanto na classe principal não será permitido
+        public double Balance { get; protected set; }
 
         public Account()
         {
@@ -25,12 +28,29 @@ namespace inheritance.Entities
 
         public void Withdraw(double amount)
         {
-            Balance -= amount;
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            } else
+            {
+                Console.WriteLine($"Saldo indisponível para saque. Limite de saque {Balance}");
+            }
         }
 
         public void Deposit(double amount)
         {
+
             Balance += amount;
+        }
+
+        public override string ToString()
+        {
+            return Number
+                + (", ")
+                + Holder
+                + (", ")
+            + Balance.ToString("F2", CultureInfo.InvariantCulture)
+                + (".");
         }
     }
 }
--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using inheritance.Entities;
+using System.Globalization;
 
 namespace inheritance
 {
@@ -10,6 +12,37 @@ namespace inheritance
     {
         static void Main(string[] args)
         {
+            BussinesAccount account = new BussinesAccount(2, "mateus", 1000, 500);
+
+            Console.WriteLine(account.Balance);
+            Console.WriteLine(account.Holder);
+            Console.WriteLine(account.Number);
+
+            Console.WriteLine(account);
+
+            Console.WriteLine("Entre com  o valor a ser sacado: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            account.Withdraw(amount);
+
+            Console.Write("Novo saldo: ");
+            Console.WriteLine(account);
+
+            Console.WriteLine("Entre com  o valor a ser depositado: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Deposit(amount);
+
+
+            Console.Write("Novo saldo: ");
+            Console.WriteLine(account);
+
+         
+
+
+            Console.ReadLine();
+
+
+
         }
     }
 }

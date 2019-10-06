fed3c8e1d9f34c390fc2f2895f08bac0ee47f333
mateusrochar <mateusrochar@gmail.com>
2019-10-06 18:24:44 -0300

    Using Upcasting and Downcastin in the main Programm

--- inheritance/Program.cs
+++ inheritance/Program.cs
@@ -12,31 +12,46 @@ namespace inheritance
     {
         static void Main(string[] args)
         {
-            BussinesAccount account = new BussinesAccount(2, "mateus", 1000, 500);
 
-            Console.WriteLine(account.Balance);
-            Console.WriteLine(account.Holder);
-            Console.WriteLine(account.Number);
-
-            Console.WriteLine(account);
-
-            Console.WriteLine("Entre com  o valor a ser sacado: ");
-            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            account.Withdraw(amount);
-
-            Console.Write("Novo saldo: ");
-            Console.WriteLine(account);
-
-            Console.WriteLine("Entre com  o valor a ser depositado: ");
-            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            account.Deposit(amount);
+            /*  trabalhando com upcasting - converter a superclasse em subclasse
+             como a subclasse é uma superclasse é perfeitamente possível sem nenhum comando adicional */
 
 
-            Console.Write("Novo saldo: ");
-            Console.WriteLine(account);
+            Account acc1 = new Account(1001, "Alex", 0.0);
+            BussinesAccount bacc1 = new BussinesAccount(1002, "Maria", 0.0, 500.00);
 
-         
+            // UpCasting
+
+            Account acc2 = bacc1;
+            // neste caso a variável criada é do tipo account, mas está instanciada como bussines account
+            Account acc3 = new BussinesAccount(1003, "Bob", 0.0,200.00);
+            Account acc4 = new SavingsAccount(1004, "Tiago", 0.0, 0.01);
+
+            // DownCasting
+
+            BussinesAccount bcc2 = (BussinesAccount)acc3;
+            bcc2.Loan(200);
+
+            //  BussinesAccount bcc3 = (BussinesAccount)acc4;
+            // testa se instanciaçaõ de acc4 é bussines account
+
+            if (acc4 is BussinesAccount)
+            {
+                // BussinesAccount bcc3 = (BussinesAccount)acc4;
+
+                // outra formar de fazer a conversão.
+                BussinesAccount bcc3 = acc4 as BussinesAccount;
+                bcc3.Loan(200.0);
+                Console.WriteLine("Loan");
+            }
+
+            if (acc4 is SavingsAccount)
+            {
+                // SavingsAccount Sacc1 = (SavingsAccount)acc4;
+                SavingsAccount Sacc1 = acc4 as SavingsAccount;
+                Sacc1.UpdateBalance();
+                Console.WriteLine("Update");
+            }
 
 
             Console.ReadLine();
